288f468325059fe7de1bf11bdddce49a89ee81db
Opu Zaman <opu.zaman@gmail.com>
2016-07-10 19:05:43 -0400

    Adding sudo to commands

--- scripts/centos-lamp.sh
+++ scripts/centos-lamp.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
 # Update CentOs with any patches
-yum update -y --exclude=kernel
+sudo yum update -y --exclude=kernel
 
 # Tools
-yum install -y nano git unzip screen
+sudo yum install -y nano git unzip screen
 
 # Apache
-yum install -y httpd httd-devel httpd-tools
-chkconfig --add httpd
-chkconfig httd on
-service httd stop
+sudo yum install -y httpd httd-devel httpd-tools
+sudo chkconfig --add httpd
+sudo chkconfig httd on
+sudo service httd stop
 
-rm -rf /var/www/html
-ln -s /vagrant /var/www/html
+sudo rm -rf /var/www/html
+sudo ln -s /vagrant /var/www/html
 
 service httd start
 
 # PHP
-yum install -y php php-cli php-common php-devel php-mysql
+sudo yum install -y php php-cli php-common php-devel php-mysql
 
 #MySQL
-yum install -y mysql mysql-server mysql-devel
-chkconfig --add mysqld
-chkconfig mysqld on
+sudo yum install -y mysql mysql-server mysql-devel
+sudo chkconfig --add mysqld
+sudo chkconfig mysqld on
 
-service mysqld start
+sudo service mysqld start
 
-mysql -u root -e "SHOW DATABASES";
+sudo mysql -u root -e "SHOW DATABASES";
 
 # Download starter content
 cd /vagrant
 sudo -u vagrant wget -q https://raw.githubusercontent.com/opuzaman21/VagrantScripts/master/files/index.html
 sudo -u vagrant wget -q https://raw.githubusercontent.com/opuzaman21/VagrantScripts/master/files/info.php
 
-server httd restart
+sudo server httd restart
