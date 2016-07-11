b7a5a028b454ae09196e232b80326acd3b167d37
Opu Zaman <opu.zaman@gmail.com>
2016-07-10 21:35:09 -0400

    updated .sh file

--- scripts/centos-lamp.sh
+++ scripts/centos-lamp.sh
@@ -11,10 +11,10 @@ sudo yum install -y httpd httpd-devel httpd-tools
 sudo systemctl start httpd
 sudo systemctl enable httpd
 sudo systemctl status httpd
-sudo systemclt stop httpd
+sudo systemctl stop httpd
 
 sudo rm -rf /var/www/html
-sudo ln -s /vagrant /var/www/html
+sudo ln -s /vagrant /var/www/html/
 
 sudo systemctl start httpd
 
@@ -22,14 +22,15 @@ sudo systemctl start httpd
 sudo yum install -y php php-cli php-common php-devel php-mysql
 
 #MySQL
-yum -y install mariadb-server mariadb
-systemctl start mariadb
-systemctl enable mariadb
+sudo yum -y install mariadb-server mariadb
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
 
-systemctl status mariadb
+sudo systemctl status mariadb
 
 # Download starter content
-cd /vagrant
+sudo yum install -y wget
+cd ~
 sudo -u vagrant wget -q https://raw.githubusercontent.com/opuzaman21/VagrantScripts/master/files/index.html
 sudo -u vagrant wget -q https://raw.githubusercontent.com/opuzaman21/VagrantScripts/master/files/info.php
 
